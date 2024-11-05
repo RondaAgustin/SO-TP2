28a5184cf8a186eba92fbf42a108510f8bd279b1
RondaAgustin <agusleonel.r@gmail.com>
2024-11-05 19:51:07 -0300

    doc: add pvs headers

--- Kernel/memoryManager/bitmap/bitmap_memory_manager.c
+++ Kernel/memoryManager/bitmap/bitmap_memory_manager.c
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include <memoryManager/bitmap/bitmap_memory_manager.h>
 
 typedef struct MemoryZone {
